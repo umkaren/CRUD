36a8f4de0312a40db903ac3cfea66139e2bb4f22
Karen Ngo <uminhngo@gmail.com>
2023-05-03 14:03:17 -0400

    added edit function and section

--- docs/crudmain.js
+++ docs/crudmain.js
@@ -1,6 +1,8 @@
 let PostList = JSON.parse(localStorage.getItem("PostList") || "[]");
 console.log(PostList);
-renderPostList();
+document.addEventListener("DOMContentLoaded", () => {
+  renderPostList();
+});
 
 // addToDO() function to do items to the list.
 function addPost() {
@@ -8,8 +10,8 @@ function addPost() {
   let author = document.querySelector("#author").value; // get input value from HTML through DOM query.
   let tags = document.querySelector("#tags").value; // Save it as an object.
 
-  const date = Date.now();
-  const timeMade = new Date(date);
+//get local date and time
+  const timeMade = new Date().toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute:'2-digit'});;
   console.log(timeMade);
 
   let eachPost = {
@@ -18,6 +20,11 @@ function addPost() {
     author: author,
     tags: tags,
   };
+
+// validates all fields must be field out for the post to be created
+if (!content || !author || !tags) {
+  alert("Please fill out all fields to post your cluck!")
+} else {
 
   // Get the updated todoList from local storage and push the added item to it.
   //todoList = JSON.parse(localStorage.getItem('todoList'));
@@ -31,6 +38,7 @@ function addPost() {
 
   // Call function to display the todoList once it gets added.
   renderPostList();
+}
 }
 
 function renderPostList() {
@@ -70,6 +78,46 @@ function renderPostList() {
       renderPostList(); // Call the function to display the changes.
     });
   });
+
+  // Event Listener for edit button to update the post content.
+  document.querySelectorAll(".js-update").forEach((updateButton, index) => {
+    updateButton.addEventListener("click", () => {
+      const eachPost = PostList[index];
+      const content = eachPost.content;
+      const tags = eachPost.tags;
+      const postElement = updateButton.closest(".card-body");
+
+      // display edit section or input fields with current post content
+      const editSection = document.querySelector(".edit-section");
+      editSection.style.display = "block";
+      document.querySelector("#edit-content").value = content;
+      document.querySelector("#edit-tags").value = tags;
+
+
+      // handle update button click
+      document.querySelector("#update-post").addEventListener("click", () => {
+        const newContent = document.querySelector("#edit-content").value;
+        const newTags = document.querySelector("#edit-tags").value;
+        if (!newContent || !newTags) {
+          alert("Please fill out all fields to complete your cluck!");
+        } else {
+          // update the post in local storage
+          PostList[index].content = newContent;
+          PostList[index].tags = newTags;
+          localStorage.setItem("PostList", JSON.stringify(PostList));
+          // close edit section and re-render the post list
+          editSection.style.display = "none";
+          renderPostList();
+        }
+      });
+
+      // handle cancel button click
+      document.querySelector("#cancel-edit").addEventListener("click", () => {
+        editSection.style.display = "none";
+      });
+    });
+  });
+
 }
 
 // Event Listener for Cluck button which calls the addPost() function when clicked.
@@ -91,8 +139,34 @@ let searchPostSection = document.querySelector(".js-search-results");
 
 searchButton.addEventListener("click", () => {
   let searchTag = document.querySelector(".search-tag").value;
-  searchPosts(searchTag);
   allPostsSection.innerHTML = "";
-  searchPostSection.innerHTML = `<p>${searchTag}</p>
-                                    <hr>`;
+  let tempPosts = JSON.parse(localStorage.getItem("PostList"));
+  console.log(tempPosts);
+  const result = tempPosts.filter((tempPosts) => tempPosts.tags.toLowerCase().includes(searchTag.toLowerCase()));
+
+  let searchResultsHTML = "";
+  for (let i = 0; i < result.length; i++) {
+    const eachPost = result[i];
+    const content = eachPost.content;
+    const author = eachPost.author;
+    const date = eachPost.timeMade;
+    const tags = eachPost.tags;
+    const html = `<div class="card mb-3"> 
+                      <div class="card-body">
+                         <div class="author-date-id">
+                           <span class="author">${author}</span> <span class="date mx-2">${date}</span>
+                           <span class="id ">ID:</span></div>
+                           <p class="card-text card-desc">${content}</p>
+                           <div class="tags container-fluid">Tags:<p class="tag-label keywords mr-3">
+                            ${tags}</p></div>
+                           <button class="btn-sm tweetbutton js-update">Update</button>
+                          <button class="btn-sm tweetbutton js-delete">Delete</button>
+                      </div>
+                    </div>
+                         `;
+
+    searchResultsHTML += html;
+  }
+
+  searchPostSection.innerHTML = searchResultsHTML;
 });
